328d918b26fdc6fcff01f829b9334233b5978eb9
JamieGatehouse <146337590+JamieGatehouse@users.noreply.github.com>
2025-11-13 12:02:33 +0000

    Update Microcontroller_Servo_Control.ino

--- Microcontroller_Servo_Control/Microcontroller_Servo_Control/Microcontroller_Servo_Control.ino
+++ Microcontroller_Servo_Control/Microcontroller_Servo_Control/Microcontroller_Servo_Control.ino
@@ -1,24 +1,39 @@
-//**********Task 1: Understanding and Controlling RC Servomotors*************
-//Implement a basic control program on a microcontroller to move an RC servomotor to specified angles
-//Demonstrate control of a single servo by setting it to various positions using PWM signals.
-//***************************************************************************
-
-
 #include <Wire.h>
 #include <Adafruit_PWMServoDriver.h>
 
+// called this way, it uses the default address 0x40
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
+// you can also call it with a different address you want
+//Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(0x41);
+// you can also call it with a different address and I2C interface
+//Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(0x40, Wire);
+
+#define SERVOMIN  90 // This is the 'minimum' pulse length count (out of 4096)
+#define SERVOMAX  490 // This is the 'maximum' pulse length count (out of 4096)
+#define SERVO_FREQ 50 // Analog servos run at ~50 Hz updates
+
+
 void setup() {
   Serial.begin(9600);
-  Serial.write("Microcontroller Servo Control");
-  
+  Serial.println("Servo angle test!");
+
   pwm.begin();
   pwm.setOscillatorFrequency(27000000);
-  pwm.setPWMFreq(50);
+  pwm.setPWMFreq(SERVO_FREQ);  // Analog servos run at ~50 Hz updates
 
   delay(10);
 }
 
-void loop() {
-  Serial.println(servoAngle);
 
+void loop() {
+  int pulselenmax = SERVOMAX;
+  pwm.setPWM(servonum, 0, pulselenmax);
+
+  delay(3000);
+
+  int pulselenmin = SERVOMIN;
+  pwm.setPWM(servonum, 0, pulselenmin);
+
+  delay(3000);
 }
+
